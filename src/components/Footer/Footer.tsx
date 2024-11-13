c1ef49e2bace32b77ec21c622fc8d8e7c66a995c
salvaKaraka <salvador.karakachoff@gmail.com>
2024-11-13 15:49:19 -0300

    fix: Update footer year and improve header accessibility

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return (
         <footer className="bg-neutral-900 text-neutral-200 text-center p-4">
-        <p className="font-semibold text-xl">© 2021 The Developer Toolkit</p>
+        <p className="font-semibold text-xl">© 2024 The Developer Toolkit</p>
         </footer>
     );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,16 +3,18 @@ import Github from "../Icons/Github";
  export default function Header() {
     return (
         <header className="border-b-2 border-neutral-700 px-4 h-12 flex items-center justify-between">
+               <a href="/">
                 <div className="flex items-end space-x-1">
                     <p className="text-orange-400 text-xl border border-orange-400 py-1 px-2" >tdt</p>
                     <div className="text-start">
                     <p className="text-neutral-500">v0.1 pre-alpha</p>
-                    <p className="font-bold text-xs">the developer toolkit</p>
+                    <p className="font-bold text-xs">The Developer Toolkit</p>
                     </div>
                 </div>
-                <div>
+                </a>
+                <a href="https://github.com/salvaKaraka" target="_blank" >
                     <Github size="30" color="#f5f5f5"  />
-                </div>
+                </a>
         </header>
     );
 }
